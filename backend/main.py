124d7f7dbeac80b375a14f56025ff1ffd58b19d8
Yassin9323 <yassin.waleed94@gmail.com>
2024-07-10 06:06:13 +0300

    editing on app.js

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 # Define the path to the static files and templates
 # static_files_path = os.path.join(current_dir, 'frontend/public')
 app.mount("/static", StaticFiles(directory="../frontend"), name="static")
-templates = Jinja2Templates(directory="../frontend")
+templates = Jinja2Templates(directory="../frontend/public")
 # Include the router from the auth module
 app.include_router(router)
 
@@ -24,12 +24,12 @@ def index():
 @app.get("/login", response_class=HTMLResponse)
 async def login_page(request: Request):
     print("Sucessssssssss")
-    return templates.TemplateResponse("login.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/dashboard", response_class=HTMLResponse)
 async def dashboard_page(request: Request):
-    return templates.TemplateResponse("dashboard.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/error", response_class=HTMLResponse)
 async def error_page(request: Request):
-    return templates.TemplateResponse("errorpage.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,43 +1,44 @@
 $(document).ready(function() {
+    console.log("Ajaxxxxxxxx")
     // Define your routes and associated stylesheets here
     const routes = {
       'hospital-dashboard': { page: 'hospitalDashboardPage', style: 'hospitalDashboard.css' },
       'blood-bank-dashboard': { page: 'bloodBankDashboardPage', style: 'bloodBankDashboard.css' },
       'login': { page: 'login', style: 'login.css' },
-      'signup': { page: 'signup', style: 'signup.css' },
+      'signup': { page: 'signupPage', style: 'signup.css' },
       '': { page: 'login', style: 'login.css' } // Default to login page
     };
   
     // Function to navigate to a page and load the corresponding stylesheet
     function navigateToPage(route) {
       const { page, style } = routes[route] || routes[''];
-      $('#root').load(`/src/pages/html/${page}.html`, function() {
-        $.getScript(`/src/pages/js/${page}.js`);
-        $('#page-style').attr('href', `/src/assets/css/${style}`);
+      $('#root').load(`/static/public/${page}.html`, function() {
+        // $.getScript(`/frontend/src/pages/js/${page}.js`);
+        $('#page-style').attr('href', `/static/src/assets/css/${style}`);
       });
     }
 
     $('#login-form').submit(function(event) {
-      event.preventDefault(); // Prevent the form from submitting the traditional way
-      var email = $('#email').val();
-      var password = $('#password').val();
-      
-      $.ajax({
-          url: '/login',
-          type: 'POST',
-          data: {email: email, password: password},
-          success: function(response) {
-              window.location.href = '/dashboard';
-          },
-          error: function(response) {
-              window.location.href = '/error';
-          }
-      });
-  });
-
+        event.preventDefault(); // Prevent the form from submitting the traditional way
+        var email = $('#email').val();
+        var password = $('#password').val();
+        
+        $.ajax({
+            url: '/login',
+            type: 'POST',
+            data: {email: email, password: password},
+            success: function(response) {
+              window.location.href = '/dashboard'
+            },
+            error: function(response) {
+              window.location.href = '/error'
+            }
+        });
+    });
   
     // // Simple routing logic
     // const path = window.location.pathname.replace('/', '');
+    // console.log(path)
     // navigateToPage(path);
   });
   
